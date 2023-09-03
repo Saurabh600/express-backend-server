d24f7448edeac52fed05d04714d111f24b29a8b8
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2023-09-03 14:34:06 +0530

    db configured with sqlite

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import "dotenv/config";
+
+import express from "express";
+import http from "http";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+import compression from "compression";
+
+import { authRouter, userRouter } from "./routes";
+
+// app initialization
+const app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || 4000;
+
+// adding middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
+app.use(morgan("tiny"));
+app.use(compression());
+
+// demo route
+app.get("/hello", (req, res) => {
+  res.status(200).json({
+    status: true,
+    info: "check health route",
+    message: "response fulfilled!",
+  });
+});
+
+// routers group
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+
+// starting server
+server.listen(Number(port), () => {
+  console.log(`application is listening at http://127.0.0.1:${port}`);
+});
